67e4d2eb3ecf363ed7c50a18e3acd3a833fd36bb
ngorbunov <nick_g@outlook.com>
2016-06-08 10:36:36 -0500

    upload sample

---
+++ server/app.js
@@ -0,0 +1,18 @@
+// require express
+var express = require ('express');
+// create a new express app
+var app = express();
+// require path
+var path = require('path');
+
+var server = app.listen( process.env.PORT ||  8080, 'localhost', function(){
+  console.log('server is listening on port 8080');
+});
+
+// base url
+app.get('/', function (req, res){
+  console.log('base url get');
+  res.writeHead(200);
+  res.write('we are in the base url');
+  res.end();
+});
